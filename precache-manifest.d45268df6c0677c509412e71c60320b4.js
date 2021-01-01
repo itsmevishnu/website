9a7cba2de3b20f67308daeac7058ce8a8e765a25
vishnu_jayan <vishnujayadhevan@yahoo.com>
2021-01-02 00:16:55 +0530

    Updates

---
+++ precache-manifest.d45268df6c0677c509412e71c60320b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c455d19907a461a8371c859a6a8f9c50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b663a5d174bab5faa97f",
+    "url": "/static/css/main.13c148f2.chunk.css"
+  },
+  {
+    "revision": "a4a179aa32f7f825df91",
+    "url": "/static/js/2.23a991eb.chunk.js"
+  },
+  {
+    "revision": "83b120d89ff9b39926c2cc432a0916ec",
+    "url": "/static/js/2.23a991eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffa0dc5aadea87a95e5f",
+    "url": "/static/js/3.9c95b2c2.chunk.js"
+  },
+  {
+    "revision": "b663a5d174bab5faa97f",
+    "url": "/static/js/main.994691bf.chunk.js"
+  },
+  {
+    "revision": "342ef7e7a3287c7d0df9",
+    "url": "/static/js/runtime-main.170357b2.js"
+  },
+  {
+    "revision": "1f845ca731dc1783ad4624cb9ef3b22a",
+    "url": "/static/media/photo.1f845ca7.jpg"
+  }
+]);
